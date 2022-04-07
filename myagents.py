66f71598b87bfb55441f110c40dde0821a1efc00
rithaniepriyanka <75235132+rithaniepriyanka@users.noreply.github.com>
2022-04-07 21:55:45 +0530

    Update myagents.py

--- myagents.py
+++ myagents.py
@@ -40,10 +40,8 @@ def TableDrivenAgentProgram(table):
     percepts = []
 
     def program(percept):
-        action =None
-        """
-        Write your code here
-        """
+        percepts.append(percept)
+        action = table.get(tuple(percepts))
         return action
 
     return program
@@ -170,10 +168,18 @@ class TrivialVacuumEnvironment(Environment):
     def execute_action(self, agent, action):
         """Change agent's location and/or location's status; track performance.
         Score 10 for each dirt cleaned; -1 for each move."""
+        if action == 'Right':
+            agent.location = loc_B
+            agent.performance -= 1
+        elif action == 'Left':
+            agent.location = loc_A
+            agent.performance -= 1
+        elif action == 'Suck':
+            if self.status[agent.location] == 'Dirty':
+                agent.performance += 10
+            self.status[agent.location] = 'Clean'
 
-        """
-        Write your code here
-        """
+
 
     def default_location(self, thing):
         """Agents start in either location at random."""
